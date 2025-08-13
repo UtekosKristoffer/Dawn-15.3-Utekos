65bf3392fa8ce1c42c8bf7553202a95d723502ca
UtekosKristoffer <santini91yt@gmail.com>
2025-08-13 12:40:01 +0200

    Create makeQueryRequest.ts

---
+++ Info/Status/makeQueryRequest.ts
@@ -0,0 +1,22 @@
+import {
+  QueryClient,
+  defaultShouldDehydrateQuery,
+} from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+      dehydrate: {
+        // include pending queries in dehydration
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === "pending",
+      },
+    },
+  });
+}
+
+export default makeQueryClient;
