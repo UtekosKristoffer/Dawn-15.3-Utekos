4a8170c9d3eaf03a6deb65647ef297ba7365c81b
UtekosKristoffer <santini91yt@gmail.com>
2025-08-13 12:38:57 +0200

    Create Providers.tsx

---
+++ Info/Status/Providers.tsx
@@ -0,0 +1,13 @@
+
+function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
