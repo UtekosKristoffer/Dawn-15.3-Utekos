bc7dfd3986ad90850715d4e80137e9c094ad063e
UtekosKristoffer <santini91yt@gmail.com>
2025-08-13 10:57:23 +0200

    Update promise-actors.ts

--- Info/xstate/promise-actors.ts
+++ Info/xstate/promise-actors.ts
@@ -102,3 +102,31 @@ export declare function fromPromise<TOutput, TInput = NonReducibleUnknown, TEmit
     signal: AbortSignal;
     emit: (emitted: TEmitted) => void;
 }) => PromiseLike<TOutput>): PromiseActorLogic<TOutput, TInput, TEmitted>;
+
+
+//toPromise.d.ts
+import { AnyActorRef, OutputFrom } from "./types.js";
+/**
+ * Returns a promise that resolves to the `output` of the actor when it is done.
+ *
+ * @example
+ *
+ * ```ts
+ * const machine = createMachine({
+ *   // ...
+ *   output: {
+ *     count: 42
+ *   }
+ * });
+ *
+ * const actor = createActor(machine);
+ *
+ * actor.start();
+ *
+ * const output = await toPromise(actor);
+ *
+ * console.log(output);
+ * // logs { count: 42 }
+ * ```
+ */
+export declare function toPromise<T extends AnyActorRef>(actor: T): Promise<OutputFrom<T>>;
