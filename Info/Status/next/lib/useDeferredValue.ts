a7a6018f5450cbe2f100864857e682793f072362
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 07:02:02 +0200

    Create useDeferredValue.ts

---
+++ Info/Status/next/lib/useDeferredValue.ts
@@ -0,0 +1,14 @@
+/**
+     * Returns a deferred version of the value that may “lag behind” it.
+     *
+     * This is commonly used to keep the interface responsive when you have something that renders immediately
+     * based on user input and something that needs to wait for a data fetch.
+     *
+     * A good example of this is a text input.
+     *
+     * @param value The value that is going to be deferred
+     * @param initialValue A value to use during the initial render of a component. If this option is omitted, `useDeferredValue` will not defer during the initial render, because there’s no previous version of `value` that it can render instead.
+     *
+     * @see {@link https://react.dev/reference/react/useDeferredValue}
+     */
+    export function useDeferredValue<T>(value: T, initialValue?: T): T;
