4f33116c8836012b0678275d00cc0fd84af9550b
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 09:24:54 +0200

    Create useFormStatus.ts

---
+++ Info/Status/newest-update/useFormStatus.ts
@@ -0,0 +1,97 @@
+//The useFormStatus Hook provides status information of the last form submission.
+
+// const { pending, data, method, action } = useFormStatus();
+
+//useFormStatus will not return status information for a <form> rendered in the same component. 
+//The useFormStatus Hook only returns status information for a parent <form> and not for any <form> rendered in the same component calling the Hook, or child components.
+
+import { useFormStatus } from "react-dom";
+import action from './actions';
+
+function Submit() {
+  const status = useFormStatus();
+  return <button disabled={status.pending}>Submit</button>
+}
+
+export default function App() {
+  return (
+    <form action={action}>
+      <Submit />
+    </form>
+  );
+}
+
+// To display a pending state while a form is submitting, you can call the useFormStatus Hook in a 
+// component rendered in a <form> and read the pending property returned.
+//Here, we use the pending property to indicate the form is submitting:
+
+import { useFormStatus } from "react-dom";
+import { submitForm } from "./actions.js";
+
+function Submit() {
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" disabled={pending}>
+      {pending ? "Submitting..." : "Submit"}
+    </button>
+  );
+}
+
+function Form({ action }) {
+  return (
+    <form action={action}>
+      <Submit />
+    </form>
+  );
+}
+
+export default function App() {
+  return <Form action={submitForm} />;
+}
+
+//You can use the data property of the status information returned from useFormStatus to display what data is being submitted by the user.
+//useFormName.js
+
+import {useState, useMemo, useRef} from 'react';
+import {useFormStatus} from 'react-dom';
+
+export default function UsernameForm() {
+  const {pending, data} = useFormStatus();
+
+  return (
+    <div>
+      <h3>Request a Username: </h3>
+      <input type="text" name="username" disabled={pending}/>
+      <button type="submit" disabled={pending}>
+        Submit
+      </button>
+      <br />
+      <p>{data ? `Requesting ${data?.get("username")}...`: ''}</p>
+    </div>
+  );
+}
+
+
+// app.js
+
+import UsernameForm from './UsernameForm';
+import { submitForm } from "./actions.js";
+import {useRef} from 'react';
+
+export default function App() {
+  const ref = useRef(null);
+  return (
+    <form ref={ref} action={async (formData) => {
+      await submitForm(formData);
+      ref.current.reset();
+    }}>
+      <UsernameForm />
+    </form>
+  );
+}
+
+
+
+
+
+
