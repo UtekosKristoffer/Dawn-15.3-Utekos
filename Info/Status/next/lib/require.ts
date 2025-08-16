97299876e36d212af9f9096c6e5d2db2b522df17
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 05:02:23 +0200

    Update require.ts

--- Info/Status/next/lib/require.ts
+++ Info/Status/next/lib/require.ts
@@ -1,3 +1,12 @@
 export declare function getMaybePagePath(page: string, distDir: string, locales: readonly string[] | undefined, isAppPath: boolean): string | null;
 export declare function getPagePath(page: string, distDir: string, locales: string[] | undefined, isAppPath: boolean): string;
 export declare function requirePage(page: string, distDir: string, isAppPath: boolean): Promise<any>;
+
+//require-hook
+export declare const hookPropertyMap: Map<any, any>;
+export declare const defaultOverrides: {
+    'styled-jsx': string;
+    'styled-jsx/style': string;
+    'styled-jsx/style.js': string;
+};
+export declare function addHookAliases(aliases?: [string, string][]): void;
