da997d88de2c66cb6e1b5063a4c8fef6d80746f3
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 04:25:48 +0200

    Update lib.ts

--- Info/Status/next/lib.ts
+++ Info/Status/next/lib.ts
@@ -533,22 +533,5 @@ export declare class ApiError extends Error {
     readonly statusCode: number;
     constructor(statusCode: number, message: string);
 }
-/**
- * Sends error in `response`
- * @param res response object
- * @param statusCode of response
- * @param message of response
- */
-export declare function sendError(res: NextApiResponse, statusCode: number, message: string): void;
-interface LazyProps {
-    req: IncomingMessage;
-}
-/**
- * Execute getter function only if its needed
- * @param LazyProps `req` and `params` for lazyProp
- * @param prop name of property
- * @param getter function to get data
- */
-export declare function setLazyProp<T>({ req }: LazyProps, prop: string, getter: () => T): void;
-export {};
+
 
