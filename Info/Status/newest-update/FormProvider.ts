7b7311a53bb11d58de7f4eeceba1bafcfeb583a9
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 09:15:32 +0200

    Create FormProvider.ts

---
+++ Info/Status/newest-update/FormProvider.ts
@@ -0,0 +1,37 @@
+import { useForm, FormProvider, useFormContext } from "react-hook-form"
+
+
+export default function App() {
+  const methods = useForm()
+
+
+  const onSubmit = (data) => console.log(data)
+  const { register, reset } = methods
+
+
+  useEffect(() => {
+    reset({
+      name: "data",
+    })
+  }, [reset]) // ❌ never put `methods` as the deps
+
+
+  return (
+    <FormProvider {...methods}>
+      // pass all methods into the context
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <NestedInput />
+        <input {...register("name")} />
+        <input type="submit" />
+      </form>
+    </FormProvider>
+  )
+}
+
+
+function NestedInput() {
+  const { register } = useFormContext() // retrieve all hook methods
+
+
+  return <input {...register("test")} />
+}
