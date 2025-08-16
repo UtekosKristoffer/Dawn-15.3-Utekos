d90a07aadbd4738b7aba6ab2bc527f4c8c0c6527
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 07:47:18 +0200

    Create getQueryClient.ts

---
+++ Info/Status/newest-update/getQueryClient.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import "server-only";
+import { QueryClient, isServer } from "@tanstack/react-query";
+import makeQueryClient from "@/Lib/Clients/makeQueryClient";
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default getQueryClient;
