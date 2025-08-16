1000937b814c78d3705bd960a5e0ed522b6bec30
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 09:30:35 +0200

    Create useActionState-guide.ts

---
+++ Info/Status/newest-update/useActionState.ts
@@ -0,0 +1,145 @@
+// useActionState is a Hook that allows you to update state based on the result of a form action.
+
+// const [state, formAction, isPending] = useActionState(fn, initialState, permalink?);
+
+
+import { useActionState } from "react";
+
+async function increment(previousState, formData) {
+  return previousState + 1;
+}
+
+function StatefulForm({}) {
+  const [state, formAction] = useActionState(increment, 0);
+  return (
+    <form>
+      {state}
+      <button formAction={formAction}>Increment</button>
+    </form>
+  )
+}
+
+
+//Call useActionState at the top level of your component to access the return value of an action from the last time a form was submitted.
+import { useActionState } from 'react';
+import { action } from './actions.js';
+
+function MyComponent() {
+  const [state, formAction] = useActionState(action, null);
+  // ...
+  return (
+    <form action={formAction}>
+      {/* ... */}
+    </form>
+  );
+}
+// illustration
+
+
+function action(currentState, formData) {
+  // ...
+  return 'next state';
+}
+
+
+// Display form errors
+// To display messages such as an error message or toast that’s returned by a Server Function, wrap the action in a call to useActionState.
+// app.js
+
+import { useActionState, useState } from "react";
+import { addToCart } from "./actions.js";
+
+function AddToCartForm({itemID, itemTitle}) {
+  const [message, formAction, isPending] = useActionState(addToCart, null);
+  return (
+    <form action={formAction}>
+      <h2>{itemTitle}</h2>
+      <input type="hidden" name="itemID" value={itemID} />
+      <button type="submit">Add to Cart</button>
+      {isPending ? "Loading..." : message}
+    </form>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <AddToCartForm itemID="1" itemTitle="JavaScript: The Definitive Guide" />
+      <AddToCartForm itemID="2" itemTitle="JavaScript: The Good Parts" />
+    </>
+  )
+}
+
+//actions.js
+
+"use server";
+
+export async function addToCart(prevState, queryData) {
+  const itemID = queryData.get('itemID');
+  if (itemID === "1") {
+    return "Added to cart";
+  } else {
+    // Add a fake delay to make waiting noticeable.
+    await new Promise(resolve => {
+      setTimeout(resolve, 2000);
+    });
+    return "Couldn't add to cart: the item is sold out.";
+  }
+}
+
+// Example 2 of 2: Display structured information after submitting a form 
+// The return value from a Server Function can be any serializable value. For example, 
+// it could be an object that includes a boolean indicating whether the action was successful, an error message, or updated information.
+//app.js
+import { useActionState, useState } from "react";
+import { addToCart } from "./actions.js";
+
+function AddToCartForm({itemID, itemTitle}) {
+  const [formState, formAction] = useActionState(addToCart, {});
+  return (
+    <form action={formAction}>
+      <h2>{itemTitle}</h2>
+      <input type="hidden" name="itemID" value={itemID} />
+      <button type="submit">Add to Cart</button>
+      {formState?.success &&
+        <div className="toast">
+          Added to cart! Your cart now has {formState.cartSize} items.
+        </div>
+      }
+      {formState?.success === false &&
+        <div className="error">
+          Failed to add to cart: {formState.message}
+        </div>
+      }
+    </form>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <AddToCartForm itemID="1" itemTitle="JavaScript: The Definitive Guide" />
+      <AddToCartForm itemID="2" itemTitle="JavaScript: The Good Parts" />
+    </>
+  )
+}
+
+//actions.js
+
+"use server";
+
+export async function addToCart(prevState, queryData) {
+  const itemID = queryData.get('itemID');
+  if (itemID === "1") {
+    return {
+      success: true,
+      cartSize: 12,
+    };
+  } else {
+    return {
+      success: false,
+      message: "The item is sold out.",
+    };
+  }
+}
+
