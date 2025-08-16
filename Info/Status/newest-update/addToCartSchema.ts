1a187752bb1c508dd33dd43403b8b95854ad7cf2
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 09:41:27 +0200

    Create addToCartSchema.ts

---
+++ Info/Status/newest-update/addToCartSchema.ts
@@ -0,0 +1,7 @@
+// Fil: src/Lib/addToCartSchema.ts
+import * as z from "zod";
+
+export const addToCartSchema = z.object({
+  variantId: z.string().min(1, "Produktvariant mangler."),
+  quantity: z.coerce.number().min(1, "Antall må være minst 1."),
+});
