fa0d7bfb95bb3ee299033d7e83d5b1bd9238517d
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 06:20:04 +0200

    Update HydrationBoundary.ts

--- Info/TanStackQuery/ts/HydrationBoundary.ts
+++ Info/TanStackQuery/ts/HydrationBoundary.ts
@@ -1,6 +1,19 @@
 'use client'
 import * as React from 'react'
 
+import * as React from 'react';
+import { DehydratedState, OmitKeyof, HydrateOptions, QueryClient } from '@tanstack/query-core';
+
+interface HydrationBoundaryProps {
+    state: DehydratedState | null | undefined;
+    options?: OmitKeyof<HydrateOptions, 'defaultOptions'> & {
+        defaultOptions?: OmitKeyof<Exclude<HydrateOptions['defaultOptions'], undefined>, 'mutations'>;
+    };
+    children?: React.ReactNode;
+    queryClient?: QueryClient;
+}
+declare const HydrationBoundary: ({ children, options, state, queryClient, }: HydrationBoundaryProps) => React.ReactElement;
+
 import { hydrate } from '@tanstack/query-core'
 import { useQueryClient } from './QueryClientProvider'
 import type {
@@ -106,3 +119,6 @@ export const HydrationBoundary = ({
 
   return children as React.ReactElement
 }
+
+
+export { HydrationBoundary, type HydrationBoundaryProps };
