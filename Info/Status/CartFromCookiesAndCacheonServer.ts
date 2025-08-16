697853b86f6cc83864e689374a6092d83303a0ea
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 02:50:24 +0200

    Update CartFromCookiesAndCacheonServer.ts

--- Info/Status/CartFromCookiesAndCacheonServer.ts
+++ Info/Status/CartFromCookiesAndCacheonServer.ts
@@ -1,19 +1,23 @@
 // Fil: src/ServerFunctions/Cart/CartFromCookiesAndCacheonServer.ts
 "use server";
-import { queryOptions } from "@tanstack/react-query";
+
+import "server-only";
 import { cookies } from "next/headers";
 import { CART_FROM_ASYNCED_COOKIESQUERY } from "@/ServerFunctions/Cart/CartFromAsyncedCookiesQuery";
+import { NextResponse } from "next/server";
+/**
+ * Henter cart fra cookies og cache, og returnerer som Promise.
+ */
 
-const CART_FROM_COOKIES_AND_CACHE_ON_SERVER = queryOptions({
-  queryKey: ["cart"],
-  queryFn: async () => {
-    const cartId = (await cookies()).get("cartId")?.value;
-    if (!cartId) {
-      return null;
-    }
-    return CART_FROM_ASYNCED_COOKIESQUERY(cartId);
-  },
-  staleTime: 1000 * 60 * 5, // 5 minutter
-});
+export async function CART_FROM_COOKIES_AND_CACHE_ON_SERVER(): Promise<Awaited<ReturnType<typeof CART_FROM_ASYNCED_COOKIESQUERY>>> {
+  const cartId = (await cookies()).get("cartId")?.value;
+  if (!cartId) {
+    return null;
+  }
+  let response = NextResponse.next();
+  response.cookies.get("cartId")?.value;
 
-export default CART_FROM_COOKIES_AND_CACHE_ON_SERVER;
+  return CART_FROM_ASYNCED_COOKIESQUERY(cartId);
+}
+
+export default CART_FROM_ASYNCED_COOKIESQUERY;
