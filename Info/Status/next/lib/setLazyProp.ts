d6a2778ef884d65721edcb9d983ef022f26f5df2
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 04:26:15 +0200

    Create setLazyProp.ts

---
+++ Info/Status/next/lib/setLazyProp.ts
@@ -0,0 +1,7 @@
+/**
+ * Execute getter function only if its needed
+ * @param LazyProps `req` and `params` for lazyProp
+ * @param prop name of property
+ * @param getter function to get data
+ */
+export declare function setLazyProp<T>({ req }: LazyProps, prop: string, getter: () => T): void;
