a21a5fb14105e732d3fb7f5af16377b572e05e44
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 09:34:05 +0200

    Create useOptimistic.ts

---
+++ Info/Status/newest-update/useOptimistic.ts
@@ -0,0 +1,82 @@
+// useOptimistic is a React Hook that lets you show a different state while an async action is underway.
+//This state is called the “optimistic” state because it is usually used to immediately present the user with the result 
+// of performing an action, even though the action actually takes time to complete.
+
+import { useOptimistic } from 'react';
+const [optimisticState, addOptimistic] = useOptimistic(state, updateFn);
+function AppContainer() {
+  const [optimisticState, addOptimistic] = useOptimistic(
+    state,
+    // updateFn
+    (currentState, optimisticValue) => {
+      // merge and return new state
+      // with optimistic value
+    }
+  );
+}
+
+
+
+
+//optimistic updating forms
+//app.js
+import { useOptimistic, useState, useRef, startTransition } from "react";
+import { deliverMessage } from "./actions.js";
+
+function Thread({ messages, sendMessageAction }) {
+  const formRef = useRef();
+  function formAction(formData) {
+    addOptimisticMessage(formData.get("message"));
+    formRef.current.reset();
+    startTransition(async () => {
+      await sendMessageAction(formData);
+    });
+  }
+  const [optimisticMessages, addOptimisticMessage] = useOptimistic(
+    messages,
+    (state, newMessage) => [
+      {
+        text: newMessage,
+        sending: true
+      },
+      ...state,
+    ]
+  );
+
+  return (
+    <>
+      <form action={formAction} ref={formRef}>
+        <input type="text" name="message" placeholder="Hello!" />
+        <button type="submit">Send</button>
+      </form>
+      {optimisticMessages.map((message, index) => (
+        <div key={index}>
+          {message.text}
+          {!!message.sending && <small> (Sending...)</small>}
+        </div>
+      ))}
+      
+    </>
+  );
+}
+
+export default function App() {
+  const [messages, setMessages] = useState([
+    { text: "Hello there!", sending: false, key: 1 }
+  ]);
+  async function sendMessageAction(formData) {
+    const sentMessage = await deliverMessage(formData.get("message"));
+    startTransition(() => {
+      setMessages((messages) => [{ text: sentMessage }, ...messages]);
+    })
+  }
+  return <Thread messages={messages} sendMessageAction={sendMessageAction} />;
+}
+
+
+// actions.js
+
+export async function deliverMessage(message) {
+  await new Promise((res) => setTimeout(res, 1000));
+  return message;
+}
