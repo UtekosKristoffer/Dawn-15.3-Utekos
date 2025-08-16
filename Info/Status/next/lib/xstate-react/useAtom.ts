e23c50cd39141d7739d6beceaebeb0e26d8db2e3
UtekosKristoffer <santini91yt@gmail.com>
2025-08-16 05:36:37 +0200

    Create useAtom.ts

---
+++ Info/Status/next/lib/xstate-react/useAtom.ts
@@ -0,0 +1,29 @@
+/**
+ * A React hook that subscribes to the `atom` and returns the current value of
+ * the atom.
+ *
+ * @example
+ *
+ * ```ts
+ * const atom = createAtom(0);
+ *
+ * const Component = () => {
+ *   const count = useAtom(atom);
+ *
+ *   return (
+ *     <div>
+ *       <div>{count}</div>
+ *       <button onClick={() => atom.set((c) => c + 1)}>Increment</button>
+ *     </div>
+ *   );
+ * };
+ * ```
+ *
+ * @param atom The atom, created from `createAtom(…)`
+ * @param selector An optional function which takes in the `snapshot` and
+ *   returns a selected value
+ * @param compare An optional function which compares the selected value to the
+ *   previous value
+ */
+export declare function useAtom<T>(atom: BaseAtom<T>): T;
+export declare function useAtom<T, S>(atom: BaseAtom<T>, selector: (snapshot: T) => S, compare?: (a: S, b: S) => boolean): S;
